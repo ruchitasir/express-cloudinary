d0149eac9aa403bc3bddb3a67d82ceff6c2ffb21
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-04-15 17:51:17 -0700

    cloudinary app

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require('dotenv').config()
+let express = require('express');
+let ejsLayouts = require('express-ejs-layouts');
+let app = express();
+let multer=require('multer')
+let upload = multer({dest: './uploads/'})
+let cloudinary = require('cloudinary')
+let imageUrl = cloudinary.url('sample',{width:150,height:150})
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+cloudinary.config(process.env.CLOUDINARY_URL)
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index',{image: imageUrl});
 });
+
+app.post('/',upload.single('myFile'),(req,res)=>{
+  cloudinary.uploader.upload(req.file.path,(result)=>{
+    res.send(result)
+  })
+ 
+})
 
 app.listen(3000);
